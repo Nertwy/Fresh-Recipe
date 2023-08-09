3cfbdc78d9ca0d64cc8d5b1ab2dd58663433fb2d
Nertwy <deniskabos@gmail.com>
2023-08-10 01:25:58 +0300

    Daisyui added

--- src/Components/MainPageComponents/Card/Card.tsx
+++ src/Components/MainPageComponents/Card/Card.tsx
@@ -8,50 +8,63 @@ import { api } from "~/utils/api";
 const Card: FC<Dishes | null> = (data) => {
   const router = useRouter();
   const { data: session } = useSession();
-  const { data: queryData, refetch} = api.main.getIsLiked.useQuery(
-    {
-      dish_id: data?.id ?? -1,
-      user_id: session?.user.id ?? "",
-    }
-  );
+  const { data: queryData, refetch } = api.main.getIsLiked.useQuery({
+    dish_id: data?.id ?? -1,
+    user_id: session?.user.id ?? "",
+  });
   // console.log(isFetchedAfterMount);
 
   if (!data) {
     return <></>;
   }
   return (
-    <div className="card-show-anim bg-slate-200 dark:bg-slate-700">
-      <div
-        className="m-3 flex w-auto cursor-pointer flex-col  rounded-md p-7 shadow-md transition duration-200  hover:z-10 hover:scale-110 hover:shadow-md dark:text-white dark:shadow-white"
-        //Cannot invoke an object which is possibly 'undefined' in TS without ?.
-      >
-        <div className="h-96 overflow-hidden">
-          <Image
-            src={data?.url ?? ""}
-            alt="Meal"
-            height={500}
-            width={500}
-            className="h-full w-full"
-            // onError={handleError}
-            onClick={() => {
-              // localStorage.setItem("food", JSON.stringify(data));
-              void router.push(`/dish/${data?.slug}`);
-            }}
-          />
-        </div>
-        <div className="h-auto w-auto flex-col">
-          <h3>
-            <b>{data?.name}</b>
-          </h3>
-          <p>{data?.cuisine}</p>
-          <div className="float-right ">
-            <HeartButton
-            refetch={refetch}
-              isLiked={queryData ?? false}
-              id={data?.id ?? -1}
-              numberOfLikes={data.likes}
-            />
-          </div>
+    // <div className="card-show-anim bg-slate-200 dark:bg-slate-700">
+    //   <div
+    //     className="m-3 flex w-auto cursor-pointer flex-col  rounded-md p-7 shadow-md transition duration-200  hover:z-10 hover:scale-110 hover:shadow-md dark:text-white dark:shadow-white"
+    //     //Cannot invoke an object which is possibly 'undefined' in TS without ?.
+    //   >
+    //     <div className="h-96 overflow-hidden">
+    //       <Image
+    //         src={data?.url ?? ""}
+    //         alt="Meal"
+    //         height={500}
+    //         width={500}
+    //         className="h-full w-full"
+    //         // onError={handleError}
+    //         onClick={() => {
+    //           // localStorage.setItem("food", JSON.stringify(data));
+    //           void router.push(`/dish/${data?.slug}`);
+    //         }}
+    //       />
+    //     </div>
+    //     <div className="h-auto w-auto flex-col">
+    //       <h3>
+    //         <b>{data?.name}</b>
+    //       </h3>
+    //       <p>{data?.cuisine}</p>
+    //       <div className="float-right ">
+    //         <HeartButton
+    //           refetch={void refetch}
+    //           isLiked={queryData ?? false}
+    //           id={data?.id ?? -1}
+    //           numberOfLikes={data.likes}
+    //         />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
